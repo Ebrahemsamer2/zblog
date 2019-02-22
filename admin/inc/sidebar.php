31d4c37aee746683156ed99e0f9c7e4890a122bb
Ebrahemsamer2 <ebrahemsamer2@gmail.com>
2019-02-22 22:12:30 +0200

    Settings Form

--- admin/inc/sidebar.php
+++ admin/inc/sidebar.php
@@ -3,7 +3,11 @@
 		<li><a href="index.php"><i class="fa fa-home"></i> Dashboard</a></li>
 		<li><a href="posts.php"><i class="fa fa-sticky-note"></i> Posts</a></li>
 		<li><a href="categories.php"><i class="fa fa-list"></i> Categories</a></li>
-		<li><a href="comments.php"><i class="fa fa-comments-o"></i> Comments</a></li>
+		<li><a href="comments.php"><i class="fa fa-comments-o"></i> Comments 
+		<?php if(get_post_comments(0)) {
+				echo "<span style='float: right;' class='badge badge-info'>". get_post_comments(0) ."</span>";	
+				} ?>
+		</a></li>
 		<li><a href="admins.php"><i class="fa fa-user-secret"></i> Admins</a></li>
 		<li><a href="tags.php"><i class="fa fa-users"></i> Users</a></li>
 		<li><a href="settings.php"><i class="fa fa-gear"></i> Settings</a></li>
